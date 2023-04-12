ea713aa89c215a78008e6adc07161ef0a462c403
trentransom <116039236+trentransom@users.noreply.github.com>
2023-04-12 12:11:07 -0400

    Adding screenshot

--- pychain.py
+++ pychain.py
@@ -192,7 +192,7 @@ if st.button("Add Block"):
     # which is set equal to a `Record` that contains the `sender`, `receiver`,
     # and `amount` values
     new_block = Block(
-        Record(sender=sender,receiver=receiver,amount=amount),
+        record=Record(sender=sender,receiver=receiver,amount=amount),
         creator_id=42,
         prev_hash=prev_block_hash
     )
